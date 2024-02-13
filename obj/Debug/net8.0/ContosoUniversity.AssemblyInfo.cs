9b548e1ef42ff470045b82e225dd876051b1f0b6
MinoolSQ <minool137@gmail.com>
2024-02-13 22:24:07 +0100

    Sort, filter, page, and group sekcija

--- obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69a9cb9b97bf397264c30a3e2a82053c1babe59b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c2b7b134dc2789fd244f42792418133a75503ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
