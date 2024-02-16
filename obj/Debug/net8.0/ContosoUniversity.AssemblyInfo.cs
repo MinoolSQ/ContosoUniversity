c6b2e15c9965997b8da887e6d40e5df004d56c78
MinoolSQ <minool137@gmail.com>
2024-02-16 17:28:56 +0100

    Realted data
    Instructor controller

--- obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+528b26c07f5e929f361dc6f9b362df2086f23e9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4dcef96057d4efaa2b4a57362503719ff7a6b1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
